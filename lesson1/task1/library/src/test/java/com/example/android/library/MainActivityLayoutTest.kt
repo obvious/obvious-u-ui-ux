930a093bf4648dddfc4942c811949d4204211894
Rohit Karadkar <karadkar.rohit@gmail.com>
2023-01-08 17:39:17 +0530

    Create a Filled button style

--- lesson1/task1/library/src/test/java/com/example/android/library/MainActivityLayoutTest.kt
+++ lesson1/task1/library/src/test/java/com/example/android/library/MainActivityLayoutTest.kt
@@ -12,7 +12,7 @@ class MainActivityLayoutTest {
     Paparazzi(deviceConfig = DeviceConfig.PIXEL_4A, theme = "AppTheme", maxPercentDifference = 0.0)
 
   @Test
-  fun layout() {
+  fun button() {
     val view = paparazzi.inflate<FrameLayout>(R.layout.activity_main)
     paparazzi.snapshot(view)
   }
