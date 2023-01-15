45d14eab694d757f870d39dbcd7ca445aa6a5a32
Rohit Karadkar <karadkar.rohit@gmail.com>
2023-01-15 12:56:27 +0530

    Add dark AppTheme

--- lesson1/task6/library/src/test/java/com/example/android/library/MainActivityLayoutTest.kt
+++ lesson1/task6/library/src/test/java/com/example/android/library/MainActivityLayoutTest.kt
@@ -3,14 +3,26 @@ package com.example.android.library
 import android.widget.FrameLayout
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
+import com.android.resources.NightMode
 import com.google.android.material.button.MaterialButton
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-class MainActivityLayoutTest {
+@RunWith(TestParameterInjector::class)
+class MainActivityLayoutTest(
+  @TestParameter config: Config
+) {
+  enum class Config(val deviceConfig: DeviceConfig) {
+    PIXEL_4A(DeviceConfig.PIXEL_4A),
+    PIXEL_4A_NIGHT(DeviceConfig.PIXEL_4A.copy(nightMode = NightMode.NIGHT)),
+  }
+
   @get:Rule
   val paparazzi =
-    Paparazzi(deviceConfig = DeviceConfig.PIXEL_4A, theme = "AppTheme", maxPercentDifference = 0.0)
+    Paparazzi(deviceConfig = config.deviceConfig, theme = "AppTheme", maxPercentDifference = 0.0)
 
   @Test
   fun button() {
