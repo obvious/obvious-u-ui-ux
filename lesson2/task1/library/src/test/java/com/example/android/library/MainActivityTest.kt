e974a8fd6096cb922605084bb9b758bfcb36e683
Rohit Karadkar <karadkar.rohit@gmail.com>
2023-01-17 21:34:35 +0530

    Use MaterialTextView for lineHeight support

--- lesson2/task1/library/src/test/java/com/example/android/library/MainActivityTest.kt
+++ lesson2/task1/library/src/test/java/com/example/android/library/MainActivityTest.kt
@@ -1,9 +1,8 @@
 package com.example.android.library
 
-import android.widget.FrameLayout
+import android.view.ViewGroup
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
-import com.android.resources.NightMode
 import com.google.testing.junit.testparameterinjector.TestParameter
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import org.junit.Rule
@@ -11,12 +10,11 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(TestParameterInjector::class)
-class MainActivityLayoutTest(
+class MainActivityTest(
   @TestParameter config: Config
 ) {
   enum class Config(val deviceConfig: DeviceConfig) {
-    PIXEL_4A(DeviceConfig.PIXEL_4A),
-    PIXEL_4A_NIGHT(DeviceConfig.PIXEL_4A.copy(nightMode = NightMode.NIGHT)),
+    PIXEL_4A(DeviceConfig.PIXEL_4A)
   }
 
   @get:Rule
@@ -24,8 +22,8 @@ class MainActivityLayoutTest(
     Paparazzi(deviceConfig = config.deviceConfig, theme = "AppTheme", maxPercentDifference = 0.0)
 
   @Test
-  fun button() {
-    val view = paparazzi.inflate<FrameLayout>(R.layout.activity_main)
+  fun layout() {
+    val view = paparazzi.inflate<ViewGroup>(R.layout.activity_main)
     paparazzi.snapshot(view)
   }
 }
