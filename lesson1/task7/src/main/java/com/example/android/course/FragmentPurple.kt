4635872df403df1247437e1c250b51b49acf2564
Rohit Karadkar <karadkar.rohit@gmail.com>
2023-01-17 09:26:58 +0530

    Remove ThemeUtils from task7

--- lesson1/task7/src/main/java/com/example/android/course/FragmentPurple.kt
+++ lesson1/task7/src/main/java/com/example/android/course/FragmentPurple.kt
@@ -1,12 +1,12 @@
 package com.example.android.course
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 class FragmentPurple : Fragment() {
@@ -27,6 +27,6 @@ class FragmentPurple : Fragment() {
         .commit()
     }
     view.findViewById<TextView>(R.id.tv_title)
-      .setTextColor(ColorStateList.valueOf(requireContext().resolveColor(R.attr.colorPrimary)))
+      .setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
   }
 }
